034f1698c6b40f302c3f788e07d1658d4ae22d7e
7Webb7 <v.dovybortsev@gmail.com>
2024-06-06 01:27:59 +0500

    check

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import telebot
+import re
 
 token = ''
 bot = telebot.TeleBot(token)
+
+pattern = re.compile(r'\(\d+(?:\s*,\s*\d+)*\)$')
 
 @bot.message_handler(commands = ['start'])
 def handle_start(message):
     bot.reply_to(message, "Приветствую, меня зовут Петрович. Текст перевернуть?")
 @bot.message_handler(content_types= 'text')
 def handle_reverse(message):
-    skip_arr = [2, 4]
+    match = pattern.search(message.text)
+    skip_arr = []
+
+    if match:
+        matched_numbers = match.group()
+        stripped_matched = re.sub(r'[(,)]', '', matched_numbers)
+
+        skip_arr = [int(i) for i in stripped_matched.split()]
+        print(skip_arr)
+
+        message.text = message.text[:match.start()].strip()
+
     mes = message.text
     mes_arr = mes.lower().split()
-    mes_arr = list(reversed(mes_arr))
+
     for i in range(len(mes_arr)):
         if i + 1 in skip_arr:
             continue
         mes_arr[i] = ''.join(reversed(mes_arr[i]))
+    mes_arr = list(reversed(mes_arr))
 
     mes_arr = ' '.join(mes_arr)
     bot.reply_to(message, mes_arr)
