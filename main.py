d4ee964485db7959649d1d724ab9d24355db0e65
7Webb7 <v.dovybortsev@gmail.com>
2024-06-05 23:43:47 +0500

    check

---
+++ main.py
@@ -0,0 +1,33 @@
+import telebot
+
+token = '6887840596:AAHkWH-WEWgXwIax5mrO5qzzzH3tgLegiEY'
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands = ['start'])
+def handle_start(message):
+    bot.reply_to(message, "Приветствую, меня зовут Петрович. Текст перевернуть?")
+@bot.message_handler(content_types= 'text')
+def handle_reverse(message):
+    skip_arr = [2, 4]
+    mes = message.text
+    mes_arr = mes.lower().split()
+    mes_arr = list(reversed(mes_arr))
+    for i in range(len(mes_arr)):
+        if i + 1 in skip_arr:
+            continue
+        mes_arr[i] = ''.join(reversed(mes_arr[i]))
+
+    mes_arr = ' '.join(mes_arr)
+    bot.reply_to(message, mes_arr)
+
+
+
+
+
+
+
+bot.polling(none_stop = True)
+
+
+
+
